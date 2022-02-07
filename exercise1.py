ba0fc4ba4ea88c751b1ca47feacff033fccf6116
Nick <nick.ciraki@gmail.com>
2022-02-07 12:58:12 +0000

    NC: Second commit

--- exercise1.py
+++ exercise1.py
@@ -5,3 +5,9 @@ firstname = input("Enter your first name: ")
 lastname = input("Enter your last name: ")
 
 print("Hello", firstname, lastname)
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
